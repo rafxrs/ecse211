96fc1e45c9f5ab7c16c7a2c7d5240764033a7bae
rafxrs <122487986+rafxrs@users.noreply.github.com>
2023-03-22 10:33:25 -0400

    edited and renamed files in week 5 folder

---
+++ final project/project/week4 tests/read_and_drop.py
@@ -0,0 +1,114 @@
+"""
+Program to read the color of the delivery zone and drop the corresponding cube
+"""
+
+from utils.brick import Motor, TouchSensor, BP, wait_ready_sensors, EV3ColorSensor 
+from utils.emergency_stop import ES
+from colors import get_color
+import time, push, select_and_push
+
+#Initialize sensors
+stopsensor = TouchSensor(1)
+slidemotor = Motor("B")
+pushmotor = Motor("C")
+zone_color_sensor = EV3ColorSensor(2)
+
+POWER_LIMIT = 40       # Power limit (percentage)
+SPEED_LIMIT = 360      # Speed limit in degree per second
+
+slidemotor.reset_encoder()                      # Reset encoder to 0 value
+slidemotor.set_limits(POWER_LIMIT, SPEED_LIMIT) # Set the power and speed limits
+slidemotor.set_power(0)
+
+wait_ready_sensors(True)
+print("Done waiting.")
+
+# amount we need to move to get to next position
+step = 98.3
+
+cube_positions = {
+    "red": 0,
+    "orange": 98.3,
+    "yellow": 196.6,
+    "green": 294.9,
+    "blue": 393.2,
+    "purple": 491.5
+}
+
+def get_cube_positions():
+    return cube_positions
+
+cubes_and_positions = {
+    "red": 0, "orange":1, "yellow":2, "green":3, "blue":4, "purple":5
+    }
+
+def get_cubes_and_positions():
+    return cubes_and_positions
+
+#Move to a specific cube position: works with color name
+def move_to_cube_position(color_name):
+    position = cube_positions[color_name] 
+    slidemotor.set_position_relative(position)
+    time.sleep(2)
+
+def move_to_base(current_color):
+    position = cube_positions[current_color]
+    slidemotor.set_position_relative(-position-1)
+    time.sleep(2)
+    
+    
+def read():
+    zone_color = get_color.get_mean_zone_color(zone_color_sensor)
+    print(get_color.get_mean_zone_color(zone_color_sensor))
+    time.sleep(1)
+    print(zone_color)
+    return zone_color
+
+#Main function
+try:
+    while not stopsensor.is_pressed():
+        #Comment out one of those lines depending on what you want to test
+        #position = int(input("What position should we move to?"))
+        zone_color = read()
+
+        #same here
+        #slidemotor.set_position_relative(382)
+        #time.sleep(2)
+        #push()
+        #slidemotor.set_position_relative(-384)
+        
+        if zone_color == "red":
+            move_to_cube_position("red")
+            push()
+            move_to_base("red")
+        
+        if zone_color == "orange": 
+            move_to_cube_position("orange")
+            push()
+            move_to_base("orange")
+
+        if zone_color == "yellow":  
+            move_to_cube_position("yellow")
+            push()
+            move_to_base("yellow")
+
+        if zone_color == "green":
+            move_to_cube_position("green")
+            push()
+            move_to_base("green")
+
+        if zone_color == "blue":
+            move_to_cube_position("blue")
+            push()
+            move_to_base("blue")
+
+        if zone_color == "purple":
+            move_to_cube_position("purple")
+            push()
+            move_to_base("purple")   
+
+    ES.emergency_stop()
+except BaseException as error:
+    print(error)
+    exit()
+
